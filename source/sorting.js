211596fbecc1391d33a029d2851f970b6799089d
Vladimir <vovasukiasyan@gmail.com>
2019-09-02 14:19:53 +0300

    Some changes

--- source/sorting.js
+++ source/sorting.js
@@ -2,12 +2,11 @@
 
 const sorting = (objects, params) => {
     objects.sort((object1, object2) => {
-        let resultOfCompare;
         for (let param of params) {
-            if (object1[param] > object2[param]) return  1;
-            else if (object1[param] < object2[param]) return resultOfCompare = -1;
+            if (object1[param] > object2[param]) return 1;
+            else if (object1[param] < object2[param]) return -1;
         }
-        return resultOfCompare;
+        return 0;
     });
     return objects;
 };
