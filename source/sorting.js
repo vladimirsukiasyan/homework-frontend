7aa580f93171d58ddca228c5fa1b5b3be12c65a1
Vladimir <vovasukiasyan@gmail.com>
2019-09-02 14:23:17 +0300

    Refactoring

--- source/sorting.js
+++ source/sorting.js
@@ -1,12 +1,11 @@
 'use strict';
 
 const sorting = (objects, params) => {
-    objects.sort((object1, object2) => {
+    return objects.sort((object1, object2) => {
         for (let param of params) {
             if (object1[param] > object2[param]) return 1;
             else if (object1[param] < object2[param]) return -1;
         }
         return 0;
     });
-    return objects;
 };
