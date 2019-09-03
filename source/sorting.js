117fb6191c6621d9f8e77d0a8da2110194016f1a
Vladimir <vovasukiasyan@gmail.com>
2019-09-03 09:49:13 +0300

    Some fix

--- source/sorting.js
+++ source/sorting.js
@@ -1,11 +1,14 @@
 'use strict';
 
 const sorting = (objects, params) => {
-    return objects.sort((object1, object2) => {
-        for (let param of params) {
-            if (object1[param] > object2[param]) return 1;
-            else if (object1[param] < object2[param]) return -1;
-        }
-        return 0;
-    });
+    if([objects,params].every(Array.isArray)){
+        objects.sort((object1, object2) => {
+            for (let param of params) {
+                if (object1[param] > object2[param]) return 1;
+                else if (object1[param] < object2[param]) return -1;
+            }
+            return 0;
+        });
+    }
+    return objects;
 };
