603403a5ee8dd89b2b9d4db7374decfb5d528e30
Vladimir <vovasukiasyan@gmail.com>
2019-09-04 00:15:35 +0300

    Refactoring using JS Standart Style

--- source/sorting.js
+++ source/sorting.js
@@ -1,14 +1,18 @@
 'use strict';
 
 const sorting = (objects, params) => {
-    if([objects,params].every(Array.isArray)){
-        objects.sort((object1, object2) => {
-            for (let param of params) {
-                if (object1[param] > object2[param]) return 1;
-                else if (object1[param] < object2[param]) return -1;
-            }
-            return 0;
-        });
-    }
+  if (![objects, params].every(Array.isArray)) {
     return objects;
+  }
+  objects.sort((object1, object2) => {
+    for (let param of params) {
+      if (object1[param] > object2[param]) {
+        return 1;
+      } else if (object1[param] < object2[param]) {
+        return -1;
+      }
+    }
+    return 0;
+  });
+  return objects;
 };
